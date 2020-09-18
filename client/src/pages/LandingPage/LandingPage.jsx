5c8b1ba8d06585bb0238b1609d641f6df8f34d4b
kkonkel23 <kailahkonkel@gmail.com>
2020-09-18 14:11:49 -0400

    add landing page

---
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LandingPage.css'
+
+function LandingPage() {
+    return (
+        <>
+            <h1>Welcome to Beets.</h1>
+            <img className="landing-logo" src="https://i.imgur.com/qx7UQ7E.png" alt=""/>
+
+        </>
+    )
+}
+
+export default LandingPage;
